e34a313e49740fb36d137ead4b2de640ed5fdba2
Ezee Sosa <sosaeze20@gmail.com>
2017-07-03 18:07:54 -0300

    ej 1.1-1.2-1.3...

---
+++ Entidades/entidades.vb
@@ -0,0 +1,3 @@
+﻿Public Class entidades
+
+End Class
---
+++ mascotas/Entidades/Loro.vb
@@ -0,0 +1,12 @@
+﻿Public Class Loro
+    Private _memoria As Queue(Of String)
+    Private _edad As Short
+    Private _fechanac As Date
+    Private _nombre As String
+    Sub New()
+        _memoria = New Queue(Of String)
+        _edad = 0
+        _fechanac = Nothing
+        _nombre = ""
+    End Sub
+End Class
